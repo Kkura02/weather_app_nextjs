46bea8fe37d2a58e6bcf8f90842df72e44a59429
Kkura02 <genofin02@gmail.com>
2024-01-23 14:53:57 +0800

    first commit

---
+++ app/components/SearchCityForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchCityForm = () => {
+  return <div>SearchCityForm</div>;
+};
+
+export default SearchCityForm;
---
+++ app/components/WeatherShow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WeatherShow = () => {
+  return <div>WeatherShow</div>;
+};
+
+export default WeatherShow;
